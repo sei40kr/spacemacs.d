fc8b87e737790be789827d7ca5ecc3f887ca6a83
Seong Yong-ju <sei40kr@gmail.com>
2019-07-07 00:10:50 +0900

    feat: change the font to menlo for powerline

--- init.el
+++ init.el
@@ -366,7 +366,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Fira Code"
+   dotspacemacs-default-font '("Menlo for Powerline"
                                :size 20
                                :weight normal
                                :width normal
