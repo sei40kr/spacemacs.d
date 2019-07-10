a81e8e2e2c34dafee340679e152dccc1244bdf56
Seong Yong-ju <sei40kr@gmail.com>
2019-07-11 00:20:01 +0900

    feat: remove key bindings for hippie-expand

--- my-auto-completion.el
+++ my-auto-completion.el
@@ -9,9 +9,3 @@
 # --
 
 $0"))
-
-(defun my/config-auto-completion ()
-  (evil-global-set-key 'insert (kbd "TAB") #'hippie-expand)
-  (evil-global-set-key 'insert (kbd "<tab>") #'hippie-expand)
-  (evil-global-set-key 'hybrid (kbd "TAB") #'hippie-expand)
-  (evil-global-set-key 'hybrid (kbd "<tab>") #'hippie-expand))
