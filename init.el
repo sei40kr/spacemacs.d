fa7a3e62459e8ea15165fd60cf481535fee379ca
Seong Yong-ju <sei40kr@gmail.com>
2019-07-16 22:04:07 +0900

    refactor: reorder neotree config variables

--- init.el
+++ init.el
@@ -64,15 +64,13 @@ values."
      ruby-on-rails
      ;; Filetree
      (neotree :variables
-              neo-confirm-create-directory 'off-p
-              neo-confirm-create-file 'off-p
-              neo-confirm-delete-directory-recursively 'off-p
-              neo-confirm-delete-file 'y-or-n-p
-              neo-confirm-kill-buffers-for-files-in-directory 'off-p
-              neo-force-change-root t
-              neo-smart-open t
               neo-theme (if (display-graphic-p) 'icons 'ascii)
-              neo-vc-integration nil)
+              neo-force-change-root t
+              neo-confirm-create-directory #'off-p
+              neo-confirm-create-file #'off-p
+              neo-confirm-delete-directory-recursively #'off-p
+              neo-confirm-delete-file #'y-or-n-p
+              neo-confirm-kill-buffers-for-files-in-directory #'off-p)
      ;; Fun
      ;; International support
      japanese
