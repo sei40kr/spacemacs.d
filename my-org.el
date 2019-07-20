6350ac219ec0c8e320610a78bd0eda6b6af9c328
Seong Yong-ju <sei40kr@gmail.com>
2019-07-20 11:12:30 +0900

    chore: remove org2blog config

--- my-org.el
+++ my-org.el
@@ -11,15 +11,5 @@
         ;; org-re-reveal
         org-re-reveal-root (concat (getenv "HOME") "/org/reveal-js"))
 
-  ;; org2blog
-  (require 'auth-source)
-  (let* ((credentials (auth-source-user-and-password "blog.yong-ju.me"))
-         (username (nth 0 credentials))
-         (password (nth 1 credentials)))
-    (setq org2blog/wp-blog-alist `("wordpress"
-                                   :url "https://blog.yong-ju.me/xmlrpc.php"
-                                   :username ,username
-                                   :password ,password)))
-
   ;; Display/update images in the buffer after I evaluate
   (add-hook 'org-babel-after-execute-hook #'org-display-inline-images t))
