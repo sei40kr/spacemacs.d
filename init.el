f24690ab812c2fed32543592a6c8609e2986155a
Seong Yong-ju <sei40kr@gmail.com>
2019-10-23 22:56:23 +0900

    feat: disable unicode symbols

--- init.el
+++ init.el
@@ -495,7 +495,7 @@ It should only modify the values of Spacemacs settings."
    ;; If non-nil unicode symbols are displayed in the mode line.
    ;; If you use Emacs as a daemon and wants unicode characters only in GUI set
    ;; the value to quoted `display-graphic-p'. (default t)
-   dotspacemacs-mode-line-unicode-symbols #'display-graphics-p
+   dotspacemacs-mode-line-unicode-symbols nil
 
    ;; If non-nil smooth scrolling (native-scrolling) is enabled. Smooth
    ;; scrolling overrides the default behavior of Emacs which recenters point
