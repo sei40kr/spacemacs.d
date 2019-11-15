3238a10a55fe93c17368d283d29b715a5973962d
Seong Yong-ju <sei40kr@gmail.com>
2019-11-16 02:09:56 +0900

    feat: customize lsp-ui-imenu colors

--- theme-custom.el
+++ theme-custom.el
@@ -6,6 +6,10 @@
         doom-themes-neotree-folder-size  1.0
         doom-themes-neotree-chevron-size 0.8
         fci-rule-color "#444444")
+  (let* ((cyan (alist-get 'cyan doom-themes--colors))
+         (green (alist-get 'green doom-themes--colors))
+         (n (if (display-graphic-p) 0 1)))
+    (setq lsp-ui-imenu-colors (list (nth n cyan) (nth n green))))
   (with-eval-after-load 'org-mode
     (doom-themes-org-config))
   (with-eval-after-load 'neotree
