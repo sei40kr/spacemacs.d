6f795c4f620ccfbf7ed4f1522a451fd8589ef6c4
Seong Yong-ju <sei40kr@gmail.com>
2019-11-23 17:51:51 +0900

    feat: add sample resources for vue

---
+++ sample-resources/vue/index.vue
@@ -0,0 +1,21 @@
+<script>
+import Counter from "./components/counter.vue";
+
+export default {
+  data() {
+    return {
+      msg: "Hello from vue-loader!"
+    };
+  },
+
+  components: {
+    Counter,
+  }
+};
+</script>
+
+<style lang="css" scoped>
+h1 {
+  display: inline;
+}
+</style>
