af7d16a5ecc29c68146828cc4782485e3329e187
Seong Yong-ju <sei40kr@gmail.com>
2020-01-04 10:55:53 +0900

    fix: fix doom themes integration with lsp-ui

--- theme-custom.el
+++ theme-custom.el
@@ -6,14 +6,19 @@
         doom-themes-neotree-folder-size  1.0
         doom-themes-neotree-chevron-size 0.8
         fci-rule-color "#444444")
-  (let* ((cyan (alist-get 'cyan doom-themes--colors))
-         (green (alist-get 'green doom-themes--colors))
-         (n (if (display-graphic-p) 0 1)))
-    (setq lsp-ui-imenu-colors (list (nth n cyan) (nth n green))))
+
+  ;; LSP
+  (with-eval-after-load 'doom-themes
+    (let* ((cyan (alist-get 'cyan doom-themes--colors))
+           (green (alist-get 'green doom-themes--colors))
+           (n (if (display-graphic-p) 0 1)))
+      (setq lsp-ui-imenu-colors `(,(nth n cyan) ,(nth n green)))))
+
+  ;; Org
   (with-eval-after-load 'org-mode
     (doom-themes-org-config))
-  ;; (with-eval-after-load 'neotree
-  ;;   (doom-themes-neotree-config))
+
+  ;; Treemacs
   (with-eval-after-load 'treemacs
     (setq doom-themes-treemacs-theme "doom-colors"
           doom-themes-treemacs-enable-variable-pitch nil)
